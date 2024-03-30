839a290b6be85fa9d7086378a3ee846ed5f745ca
wendymasese <wendy.masese@student.moringaschool.com>
2024-03-30 16:04:34 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(a, b) {
+    return a / b;
+};
+
+// Arrow function called square
+const square = (num) => {
+    return num * num;
+};
+
+// Arrow function called add
+const add = (x, y) => {
+    return x + y;
+};
